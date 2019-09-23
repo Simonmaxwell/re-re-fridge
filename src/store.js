7ead599e8a9aec703d5ab82f5d486e3a68e31bc8
The Good Maxwell <maxwellraden@gmail.com>
2019-09-23 17:19:52 -0500

    installed axios, added state

--- src/store.js
+++ src/store.js
@@ -4,7 +4,16 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: { 
+  	appState: "WelcomeScreen", 
+  	desiredIngredients: [], 
+  	repulsiveIngredients: [], 
+  	resultRecipes: []
+  },
   mutations: {},
-  actions: {}
+  actions: {
+  	findRecipesFromIngredients({commit, state}) {
+
+  	}
+  }
 });
