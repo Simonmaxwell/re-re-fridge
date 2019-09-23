53ecc4daeda5f15015fb3a39c66ac3b2e3e2f10a
The Good Maxwell <maxwellraden@gmail.com>
2019-09-23 17:45:28 -0500

    GET request WIP

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Axios from "axios";
 
 Vue.use(Vuex);
 
@@ -13,6 +14,29 @@ export default new Vuex.Store({
   mutations: {},
   actions: {
   	findRecipesFromIngredients({commit, state}) {
+    	const APIKEY = "f164427952ade2ca59a10717ddb7ecc2";
+    	const APPID =  "1a6c0d8f";
+    	const URL = "https://api.edamam.com/search";
+
+  		axios.get(URL, {
+  			params: {
+  			app_id: APPID,
+            app_key: APIKEY,
+            from: 0,
+            to: 50,
+            q: query
+
+  			}
+  		})
+  		.then(function (response) {
+  			console.log(response);
+  		})
+  		.catch(function (error) {
+  			console.log(error);
+  		})
+  		.finally(function () {
+  			// always executed
+  		});
 
   	}
   }
